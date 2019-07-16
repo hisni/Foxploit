489ef6870829b095851865ebbe804123e2602aa9
luk3Sky <ganindudananja@gmail.com>
2019-07-16 10:47:24 +0530

    Components arranged

--- foxploit-homepage/src/App.js
+++ foxploit-homepage/src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from './components/Menu';
+import Team from './components/Team';
+
+// <!-- Bootstrap CSS -->
+import './assets/css/bootstrap.min.css';
+import './assets/css/animate.css';
+import './assets/css/magnific-popup.css';
+import './assets/css/themify-icons.css';
+import './assets/css/magnific-popup.css';
+import './assets/css/style.css';
+// import './assets/css/owl.carousel.min.css';
+// import './assets/css/flaticon.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="main_menu home_menu">
+          <Menu />
+          <Team />
       </header>
     </div>
   );
